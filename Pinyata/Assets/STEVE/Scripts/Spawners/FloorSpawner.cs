57904d25da5ddfc2685b8171c4d2e96ea98853b8
Steve <stephen.wilson@beds.ac.uk>
2017-01-20 20:06:56 +0000

    FirstScene script floor
    Added scripted floor tile systems

---
+++ Pinyata/Assets/STEVE/Scripts/Spawners/FloorSpawner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorSpawner : MonoBehaviour
+{
+    public GameObject floorTilePrefab;
+    // Use this for initialization
+    public int width = 8, height = 8;
+    void Start()
+    {
+        genFloor();
+    }
+    public void genFloor()
+    {
+        for (int i = 0; i <= width; i++)
+        {
+            for (int j = 0; j <= height; j++)
+            {
+                float x = i - (width * 0.5f);
+                float z = j - (height * 0.5f);
+                Instantiate(floorTilePrefab, new Vector3(x,0,z), Quaternion.identity);
+                Debug.Log("Floor Tile coords = "+ x + " 0 " + z);
+            }
+        }
+    }
+
+}
